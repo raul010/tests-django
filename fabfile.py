e4f107310a43f21fd0d7d8ab5a0033ef15b1a0e1
Raul Abreu Leite <raul.abreu.leite@gmail.com>
2014-03-26 00:05:17 -0300

    Auto Commit

--- fabfile.py
+++ fabfile.py
@@ -11,11 +11,17 @@ git_env = '/home/raul/dev/works-py2/tests-django/'
 
 def deploy(remote, origin='heroku'):
     local('pip freeze > requirements.txt')
-    setup(deploy_env, origin, remote)
+
+    with settings(warn_only=True):
+        setup(deploy_env, origin, remote)
+        # local('heroku maintenance:on')
+        local('git push -f heroku master')
+    # local('heroku maintenance:off')
 
 def git(remote, origin='origin'):
-    setup(git_env, origin, remote)
-    local('git push -u origin master')
+    with settings(warn_only=True):
+        setup(git_env, origin, remote)
+        local('git push -u origin master')
 
 def setup(env, origin, remote):
     with lcd(env):
@@ -26,22 +32,17 @@ def setup(env, origin, remote):
         local('git init')
         local('git add .')
 
-        with settings(warn_only=True):
-            print("*** Enter your Commit Comment: ")
-            comment = raw_input()
-            if not comment:
-                comment = 'Auto Commit'
+        print("*** Enter your Commit Comment: ")
+        comment = raw_input()
+        if not comment:
+            comment = 'Auto Commit'
 
-                local('git commit -m "%s"' % comment)
+            local('git commit -m "%s"' % comment)
 
-            error = 128;
-            result = local('git remote add %s %s' %(origin, remote))
-            if result.return_code == error:
-                print("*** Remote already exists. Command ignored. ")
-
-            # local('heroku maintenance:on')
-            local('git push -f heroku master')
-            # local('heroku maintenance:off')
+        error = 128;
+        result = local('git remote add %s %s' %(origin, remote))
+        if result.return_code == error:
+            print("*** Remote already exists. Command ignored. ")
 
 def remove_pyc_files():
     local('find . -name "*.pyc" -delete')
